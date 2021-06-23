b3d652eba1b90ef00693856f0ca5aae7fcc74190
Marco D'Amelio <Marco@pasquales-mbp.speedport.ip>
2021-06-23 20:20:36 +0200

    Anpassung der Funktionen, laufen alle bis auf prompt, Beispiel im Skript läuft bis prompt korrekt durch

--- Marco'sOrdner/MiniL2.hs
+++ Marco'sOrdner/MiniL2.hs
@@ -1,26 +1,60 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module MiniL2 where
 
     import Parser
     import Tokenizer
     import Models
     import Data.List
+    import Control.Monad
 
+    --Wichtig bei der Ausgabe eines (c,r,t,p),b,stack (Beispiel zur Verdeutlichung)
+    --Zielprogramm: ["unify p","backtrackQ","push q","call","backtrackQ","returnL","unify q","backtrackQ","push r","call","backtrackQ","returnL","unify r","backtrackQ","returnL","push p","call","backtrackQ","push r","call","backtrackQ","prompt"]
+    --C zeigt auf eine Klausel im Zielprogramm sprich (C 0) ist die erste Klausel im Zielprogramm.
+    --R zeigt auf ein Element im Stack (C 5) heißt also das 6ste Element im Stack, dieses zeigt wieder auf einen Punkt im Stack (C 0) entspricht also das erste Element im Stack
+    --T zeigt auf den Top des stacks. (C 7) heißt also der Stack hat 8 Elemente
+    --P zeigt auf eine Klausel im Zielprogramm, und zwar die nächste die ausgeführt wird
+    -- ((C 4, C 5, C 7, C 3), False, [Just (I (C 6)),Just (I Nil),Just (I (C 18)),Just (A "p"),Just (I (C 0)),Just (I (C 0)),Just (I (C 5)),Just (A "q")]), entspricht also:
+    -- C = C 4 = 5tes Element des Stacks = Just (I (C 0)) = Zeigt auf die erste Klausel
+    -- R = C 5 = 6stes Element des Stacks = Just (I (C 0)) = Zeigt auf das erste Element im Stack
+    -- T = C 7 = Zeigt auf das Achte Element des Stacks also = Just (A "q")
+    -- P = C 3 = Zeigt auf die vierte Anweisung. sprich "call" 
+
+
+    --TODO Pointer entspricht momentan den Pointern auf den Stack und worauf der Stack zeigt, im Stack ist jedes (4x + 2)te Elememt ein Pointer auf den Stack (C 0 meint also das erste Element des Stacks.)
+
+    data Pointer      = Nil | C Integer deriving (Show)
+    data StackElement = A Atom | I Pointer deriving (Show)--Elements, able to be in our Stack 
+
+    instance Eq Pointer where 
+        C x == C y = x == y
+        _   == _   = False
+
+    instance Ord Pointer where
+        compare (C x) (C y) = compare x y
+        compare Nil   (C y) = LT 
+        compare (C x)  Nil  = GT 
+        compare Nil    Nil  = EQ
+
+    instance Eq StackElement where
+        (A x) == (A y) = x == y
+        (I x) == (I y) = x == y
+        _     == _     = False
 
     type B = Bool --backtrack flag
-    type T = Integer  --Top of Stack
-    type C = Integer  --next Klausel
-    type R = Integer  --backtrack adress
-    type P = Integer  --programmcounter for next command
+    type T = Pointer  --Top of Stack
+    type C = Pointer  --next Klausel
+    type R = Pointer  --backtrack adress
+    type P = Pointer  --programmcounter for next command
     type Atom = String -- for MiniL, could be Variable or Name
 
     type I = (Adressreg, B, Stack) -> (Adressreg, B,  Stack) --Instructionregister, holds one Instruction
     type Zielcode = [String] --Goal after using ubersetzungen
 
     type Stack = [Maybe StackElement] --Stack
-    data StackElement = A Atom | I Integer deriving (Show, Eq)--Elements, able to be in our Stack 
+
 
     type Adressreg = (T, C, R, P) --Pointers that look at different points in Stack
+
+
 
     --Übersetzung für MiniL Programme
     uebersetzungMiniL :: Tree -> Zielcode -> Zielcode
@@ -41,138 +75,188 @@ module MiniL2 where
     uebersetzungBody                                            _         _ = error "Wird noch nicht behandelt"
 
     --Zielcode wird weitergegeben, da manche Aufrufe, die folgen, den Zielcode benötigen(Bsp: push ruft cFirst auf und das braucht den Zielcode.)
-    stringToCommand:: Zielcode -> Zielcode -> I
-    stringToCommand ("prompt":xs)     zielcode = prompt zielcode
-    stringToCommand ("returnL":xs)    zielcode = returnL zielcode
-    stringToCommand ("backtrackQ":xs) zielcode = backtrackQ zielcode
-    stringToCommand ("call":xs)       zielcode = call zielcode
-    stringToCommand ((x:xs):rest)     zielcode
-                        | x == 'u'             = unify (snd'(words xs)) zielcode
-                        | x == 'p'             = push (snd'(words xs)) zielcode
-                        | otherwise            = error "Nah fam"
-    stringToCommand _                 _        = error "Was kam den da vor?"
+    stringToCommand:: Zielcode -> Pointer -> Zielcode -> I
+    stringToCommand           []        _            _ = error "Kein Zielcode"
+    stringToCommand ("prompt":xs)       (C 0) zielcode = prompt zielcode
+    stringToCommand ("returnL":xs)      (C 0) zielcode = returnL zielcode
+    stringToCommand ("backtrackQ":xs)   (C 0) zielcode = backtrackQ zielcode
+    stringToCommand ("call":xs)         (C 0) zielcode = call zielcode
+    stringToCommand ((x:xs):rest)       (C 0) zielcode
+                        | x == 'u'                     = unify (snd'(words xs)) zielcode
+                        | x == 'p'                     = push (snd'(words xs)) zielcode
+                        | otherwise                    = error "Nah fam"
+    stringToCommand            (x:xs) auswahl zielcode = stringToCommand xs (addPI auswahl(-1)) zielcode
 
     snd':: [a] -> a
     snd' = head . tail
+    
+    -- plus Funktionen für den Typ Pointer
+    addPP :: Pointer -> Pointer -> Pointer
+    addPP Nil Nil = Nil
+    addPP Nil x   = x
+    addPP x   Nil = x
+    addPP (C x) (C y) = C (x+y)
 
+    addPI :: Pointer -> Integer -> Pointer 
+    addPI Nil      x = (C x)
+    addPI (C y) x = (C (y+x))
+
+    --Stack und Registerverändernde Operationen
     push:: Atom -> Zielcode -> I
-    push atom zielcode ((c,r,t,p),b,stack) = ( (t+1, c+1, t+4, p+1),
+    push atom zielcode ((c,r,t,p),b,stack) = ( (addPI t 1, addPI t 2, addPI t 4, addPI p 1),
                                                b,
-                                               stack ++ [Just (I (cFirst zielcode)),Just (I c), Just (I (p+3)), Just (A atom)]
+                                               stack ++ [Just (I (cFirst zielcode)),Just (I c), Just (I (addPI p 3)), Just (A atom)]
                                              )
 
     unify :: Atom -> Zielcode -> I
-    unify atom zielcode ((c,r,t,p),b,stack) =( (c,r,t,p+1),
-                                               isThere (c+3) (Just (A atom)) stack,
+    unify atom zielcode ((c,r,t,p),b,stack) =( (c,r,t,addPI p 1),
+                                               not(isThere (addPI c 3) (Just (A atom)) stack),
                                                 stack
                                               )
 
     call :: Zielcode -> I
-    call zielcode ((c,r,t,p),b,stack) = if isThere c Nothing stack
+    call zielcode ((c,r,t,p),b,stack) = if isThere c (Just (I Nil)) stack
                                         then (
-                                             (c,r,t,p+1),
+                                             (c,r,t, addPI p 1),
                                              True,
                                              stack
                                              )
                                         else (
-                                            (c,r,t, search c stack),
+                                            (c,r,t, searchStack c stack),
                                             b,
-                                            replace c (Just (I (cNext c zielcode))) stack []
+                                            replace c (Just (I (cNext (searchStack c stack) zielcode))) stack []
                                             )
 
     returnL :: Zielcode -> I
-    returnL zielcode ((c,r,t,p),b,stack) = if not(isThere r Nothing stack)
+    returnL zielcode ((c,r,t,p),b,stack) = if not(isThere r (Just (I Nil)) stack)
                                            then (
-                                                (c,search r stack+1, t, search (r+1) stack),
+                                                (c,(addPI (searchStack r stack) 1), t, (searchStack (addPI r 1) stack)),
                                                 b,
                                                 stack
                                                 )
                                            else (
-                                                (c,r,t, search (r+1) stack),
+                                                (c,r,t, (searchStack (addPI r 1) stack)),
                                                 b,
                                                 stack
                                                 )
 
     backtrackQ :: Zielcode -> I
     backtrackQ zielcode ((c,r,t,p),b,stack) = if b
-                                              then if isThere c Nothing stack && not(isThere r Nothing stack)
-                                                   then backtrackQ zielcode ((search r stack, c+1, c+3, p),b, stack)
-                                                   else if isThere c Nothing stack
+                                              then if isThere c (Just (I Nil)) stack && not(isThere r (Just (I Nil)) stack)
+                                                   then backtrackQ zielcode (((searchStack r stack), (addPI (searchStack r stack) 1), (addPI (searchStack r stack) 4), p),b, stack)
+                                                   else if isThere c (Just (I Nil)) stack
                                                         then ((c,r,t,cLast zielcode), b, stack)
-                                                        else ((c,r,t, search c stack), False, replace c (Just (I (cNext c zielcode))) stack [])
+                                                        else ((c,r,t, searchStack c stack), False, replace c (Just (I (cNext (searchStack c stack) zielcode))) stack [])
                                               else (
-                                                   (c,r,t,p+1),
+                                                   (c,r,t,addPI p 1),
                                                    b,
                                                    stack
                                                    )
-
+    
+    --Prompt nicht korrekt
     prompt :: Zielcode -> I
-    prompt zielcode ((c,r,t,p),b,stack)
-          | b     = undefined
-          | not b = undefined
+    prompt zielcode tupel@((c,r,t,p),b,stack)
+          | b == False = tupel
+          | b == True  = ((c,r,t,Nil),b,stack)
+    
+    promptY :: Zielcode -> I 
+    promptY zielcode ((c,r,t,p),b,stack) = ((c,r,t, addPI p (-1)), True, stack)
 
-    prompt':: IO()
-    prompt' = undefined
+    promptN :: Zielcode -> I 
+    promptN zielcode ((c,r,t,p),b,stack) = ((c,r,t,Nil),b,stack)
 
-    --Anmerkung: 0 = Nil, so the Stack starts at 1 not at 0. Index 0 doesnt exits and will be used for errors..
-    cFirst :: Zielcode -> Integer
+    --Hilfsoperationen für env, geben alle einen Pointer zurück, der auf ein Element im Stack zeigt
+    --returns the index of the first command of the first Pk
+    cFirst :: Zielcode -> Pointer
     cFirst     [] = error "Something went wrong whilst translating"
-    cFirst (x:xs) = cFirst' (x:xs) [] 1
-
-    --should return the index of the first command from the programms first Programmklausel
-    cFirst':: Zielcode -> Zielcode -> Integer -> Integer
-    cFirst'              [] akk index = undefined
-    cFirst' ("returnL":xs) akk index = undefined
-    cFirst'         (x:xs) akk index = undefined
+    cFirst (x:xs) = C 0
 
     --returns the index of the first command of the c+1-(fst/snd/thrd/th) Programmklausel 
-    cNext:: Integer -> Zielcode -> Integer
-    cNext c     [] = error "Something went wrong whilst translating2"
-    cNext c (x:xs) = cNext' c (x:xs) [] 1
-
-    cNext' :: Integer -> Zielcode -> Zielcode -> Integer -> Integer
-    cNext' c            []  akk zahl = 0 -- This means Nil
-    cNext' c ("returnL":xs) akk zahl = cNext' c xs ("returnL":akk) (zahl+1)
-    cNext' c         (x:xs) akk zahl = if c == toInteger(length akk) then zahl else cNext' c xs akk (zahl+1)
+    cNext:: Pointer -> Zielcode -> Pointer
+    cNext c         [] = error "Something went wrong whilst translating2"
+    cNext Nil     ziel = Nil
+    cNext c@(C x) ziel
+            | c > letzteKlausel (cLast ziel) (reverse ziel) [] = Nil 
+            | otherwise                                         = lookForReturn (drop (fromInteger x) ziel) (x+1)
 
     --returns the index of the last command(always prompt)
-    cLast:: Zielcode -> Integer
+    cLast:: Zielcode -> Pointer
     cLast     [] = error "something went wrong whilst translating3"
-    cLast (x:xs) = toInteger (length (x:xs))
+    cLast (x:xs) = C $ (toInteger (length (x:xs)) -1)
 
     --Returns the index of the first command of the programms goal 
-    cGoal :: Zielcode -> Integer
+    cGoal :: Zielcode -> Pointer
     cGoal     [] = error "something went wrong whilst translating4"
     cGoal (x:xs) = cGoal' (reverse(x:xs)) (toInteger (length (x:xs)))
 
-    cGoal' :: Zielcode -> Integer -> Integer
+    cGoal' :: Zielcode -> Integer -> Pointer
     cGoal' []             index = error "No Goal found."
-    cGoal' ("returnL":xs) index = index+1
+    cGoal' ("returnL":xs) index = C (index)
     cGoal' (x:xs) index = cGoal' xs (index-1)
 
     --replaces StackElement at the Index Stelle of Stack with newElement. (stack[stelle] = newElement) 
-    replace :: Integer -> Maybe StackElement -> Stack -> Stack -> Stack
-    replace 0      newElement (x:xs)            akk = akk++[newElement]++ xs
-    replace stelle newElement []                akk = error "Stack went empty whilst replaceing"
-    replace stelle newElement (Nothing:xs)      akk = replace (stelle-1) newElement xs (akk++[Nothing])
-    replace stelle newElement ((Just (A x)):xs) akk = replace (stelle-1) newElement xs (akk++[Just (A x)])
-    replace stelle newElement ((Just (I x)):xs) akk = replace (stelle-1) newElement xs (akk++[Just (I x)])
+    replace :: Pointer -> Maybe StackElement -> Stack -> Stack -> Stack
+    replace _        _          []  akk = error "Stack went empty whilst replaceing"
+    replace Nil      _          _   akk = error "Can't replace at Stelle Nil"
+    replace (C 0) newElement (x:xs) akk = akk++[newElement]++ xs
+    replace (C z) newElement (x:xs) akk = replace (C (z-1)) newElement xs (akk++[x])
 
     --returns the pointer of Stack at Index stelle. (stack[stelle])
-    search :: Integer -> Stack -> Integer
-    search 0      ((Just (A x)):xs) = error "Stack contained an Atom and not a Integer"
-    search 0      ((Just (I x)):xs) = x
-    search stelle []                = error "Stack went empty whilst searching"
-    search stelle (Nothing:xs)      = search (stelle-1) xs
-    search stelle ((Just (A x)):xs) = search (stelle-1) xs
-    search stelle ((Just(I x)):xs)  = search (stelle-1) xs
+    searchStack :: Pointer -> Stack -> Pointer
+    searchStack Nil      stack             = error "Can't search for Stelle Nil"
+    searchStack stelle   []                = error "Stack went empty whilst searching"
+    searchStack (C 0) ((Just (A x)):xs) = error "Stack contained an Atom and not a Integer"
+    searchStack (C 0) ((Just (I x)):xs) = x
+    searchStack (C 0) (Nothing:xs)      = Nil
+    searchStack (C z) (Nothing:xs)      = searchStack (C (z-1)) xs
+    searchStack (C z) ((Just (A x)):xs) = searchStack (C (z-1)) xs
+    searchStack (C z) ((Just (I x)):xs) = searchStack (C (z-1)) xs
 
     --returns True if StackElement is at Index stelle. (stack[stelle] == gesucht)
-    isThere :: Integer -> Maybe StackElement -> Stack -> Bool
-    isThere 0      gesucht (x:xs)            = gesucht == x
-    isThere stelle gesucht []                = False
-    isThere stelle gesucht (Nothing:xs)      = isThere (stelle-1) gesucht xs
-    isThere stelle gesucht ((Just (A x)):xs) = isThere (stelle-1) gesucht xs
-    isThere stelle gesucht ((Just(I x)):xs)  = isThere (stelle-1) gesucht xs
+    isThere :: Pointer -> Maybe StackElement -> Stack -> Bool
+    isThere Nil      _       _         = error "Can't lookup Nil-te Stelle"
+    isThere stelle gesucht []          = False
+    isThere  (C 0) gesucht (x:xs)      = gesucht == x
+    isThere  (C z) gesucht (x:xs)      = isThere (C (z-1)) gesucht xs
+
+    --Hilfsfunktion fuer cNext
+    lookForReturn :: Zielcode -> Integer -> Pointer
+    lookForReturn []             zahl = Nil
+    lookForReturn ("returnL":xs) zahl = C zahl
+    lookForReturn (x:xs)         zahl = lookForReturn xs (zahl+1)
+
+    --Hilfsfunktion fuer cNext
+    letzteKlausel :: Pointer -> Zielcode -> Zielcode -> Pointer
+    letzteKlausel c            []              akk = Nil
+    letzteKlausel c  ("returnL":xs)             [] = letzteKlausel (addPI c (-1)) xs ["returnL"] 
+    letzteKlausel c  ("returnL":xs)    ["returnL"] = c
+    letzteKlausel c          (x:xs)            akk = letzteKlausel (addPI c (-1)) xs akk
+
+    --shows the Command supposed to be next in I at Point I
+    showCommand :: Zielcode -> (Adressreg,B,Stack) -> Pointer -> String
+    showCommand zielcode@(string:rest) zustand@((c,r,t,p),b,stack) x 
+                                           | (C 0) == x = head zielcode
+                                           | (C (toInteger (length(zielcode)))) <= x = error "Suchanfrage out of range"
+                                           | x < (C 0)  = error "Keine Negativen Suchanfragen"
+                                           | otherwise  = showCommand rest zustand (addPI x (-1))
 
 
+    --Testing Stuff
+    befehlsZyklus :: Zielcode -> I
+    befehlsZyklus zielcode tupel@((c,r,t,p),b,stack) 
+                                    | p == Nil  = tupel
+                                    | showCommand zielcode tupel p == "prompt" = tupel
+                                    | otherwise = befehlsZyklus zielcode (stringToCommand zielcode p zielcode tupel)
+
+
+    
+ 
+    main :: IO()
+    main = do
+             let zielcode = uebersetzungMiniL(parser(tokenizer "p:-q.\nq:-r.\nr.\n:-p,r." [])) []
+             let start = ((Nil, Nil, C (-1), (cGoal zielcode)), False, [])
+             let tupel@((c,r,t,p),b,stack) = befehlsZyklus zielcode start
+             when (b) (putStrLn "no (more) solutions")
+             input <- getChar 
+             when (input == ';') (print (befehlsZyklus zielcode ((c,r,t,(addPI p (-1))),True,stack)))
+             putStrLn "Done"
