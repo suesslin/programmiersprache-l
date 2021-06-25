c540f512fc68c2c2db7098731eefa3f1f26fca4e
Max <seilmaierm@gmail.com>
2021-06-25 22:45:14 +0200

    fix stackInsertAtLocation, polish, change signatures

--- src/Stack.hs
+++ src/Stack.hs
@@ -18,13 +18,10 @@ instance Monoid (Stack a) where
 instance Functor Stack where
   fmap f (Stack content) = Stack (map f content)
 
--- special show instance for Stack, for easier debugging. TODO: doesnt work, needs a zip
-instance (Show a, Eq a) => Show (Stack a) where
-  show (Stack stackelems) = stackShow $ zip [0 ..] stackelems
-    where
-      stackShow :: (Eq a, Show a) => [(Int, a)] -> String
-      stackShow items = unlines $ map (\(x, y) -> "s" ++ show x ++ ": " ++ show y) items
 
+instance (Show a) => Show (Stack a) where
+  show (Stack stackelems) = unlines $ zipWith (\x y -> "s" ++ show x ++ ": " ++ show y) [0 ..] stackelems
+  
 stackNewEmpty :: Stack a
 stackNewEmpty = Stack []
 
@@ -34,29 +31,29 @@ stackPeekTop (Stack content) = last content
 stackPop :: Stack a -> Stack a
 stackPop (Stack content) = Stack (init content)
 
-stackPush :: Stack a -> a -> Stack a
-stackPush (Stack content) new = Stack (reverse $ new : content)
-
+stackPush :: a -> Stack a -> Stack a
+stackPush new (Stack content) = Stack (reverse $ new : reverse content)
 -- suboptimal, quick and dirty solution for Inifite Type Problem when new is appended to the end of content, NEEDS REWORK
 
 stackSizeOf :: Stack a -> Int
 stackSizeOf (Stack content) = length content
 
--- TODO: Change order: Int -> Stack a -> a
-stackItemAtLocation :: Stack a -> Int -> a
-stackItemAtLocation (Stack content) 0   = head content
-stackItemAtLocation (Stack content) pos = content !! pos
+stackItemAtLocation :: Int -> Stack a -> a
+stackItemAtLocation 0 (Stack content)    = head content
+stackItemAtLocation pos (Stack content)  = content !! pos
 
-stackWithReplacedItemAt :: Stack a -> Int -> a -> Stack a
-stackWithReplacedItemAt (Stack content) pos val
-  | pos <= (length content -1) = Stack (take pos content) <> Stack [val] <> Stack (drop (pos + 1) content)
+stackInsertAtLocation :: Int -> a -> Stack a ->  Stack a
+stackInsertAtLocation pos val (Stack []) = Stack [val]
+stackInsertAtLocation pos val (Stack content@(x:xs)) 
+  | -1 <= pos && pos <= 0 = Stack[val] <> Stack content
+  | pos <= length content -1 = Stack[x] <> stackInsertAtLocation (pos-1) val (Stack xs)
   | otherwise = error "position exceeds listsize."
 
-stackLocationFirstItemOfKind :: (Eq a) => Stack a -> a -> Int
-stackLocationFirstItemOfKind (Stack content) item = fst $ head $ filter ((== item) . snd) $ zip [0 ..] content
+stackLocationFirstItemOfKind :: (Eq a) => a -> Stack a -> Int
+stackLocationFirstItemOfKind item (Stack content) = fst $ head $ filter ((== item) . snd) $ zip [0 ..] content
 
-stackLocationLastItemOfKind :: (Eq a) => Stack a -> a -> Int
-stackLocationLastItemOfKind (Stack content) item = fst $ last $ filter ((== item) . snd) $ zip [0 ..] content
+stackLocationLastItemOfKind :: (Eq a) => a -> Stack a -> Int
+stackLocationLastItemOfKind item (Stack content) = fst $ last $ filter ((== item) . snd) $ zip [0 ..] content
 
 stackTake :: Int -> Stack a -> Stack a
 stackTake n (Stack a) = Stack $ take n a
